37af7c60c9821bb5a85401e3b26d3b21cf5253eb
juanfal <juan.falgueras@gmail.com>
2025-06-24 13:29:42 +0200

    Update zshrc.zsh

--- zshrc.zsh
+++ zshrc.zsh
@@ -41,3 +41,63 @@ if type brew &>/dev/null; then
   brew_path="$(brew --prefix)/share/zsh/site-functions"
   [[ ":$FPATH:" != *":$brew_path:"* ]] && FPATH="$brew_path:$FPATH"
 fi
+
+
+webman() { open "http://mirrors.ccs.neu.edu/cgi-bin/unixhelp/man-cgi?$1"; }
+
+xmanpage() {
+  local cmd="man"
+  [[ -n $2 ]] && cmd+=" $2"
+  cmd+=" $1"
+
+  osascript <<EOF
+tell application "Terminal"
+  activate
+  set win to (make new window)
+  do script "$cmd" in front window
+  set current settings of front window to settings set "Man Page"
+  set bounds of front window to {200, 100, 1200, 800}
+end tell
+EOF
+}
+closeman() {
+  osascript -e 'tell application "Terminal" to close (every window where name of current settings of every tab contains "Man Page")'
+}
+
+
+tman () { MANWIDTH=160 MANPAGER='col -bx' man "$@" | subl ; }
+
+
+
+
+function mann() {
+  if [[ -z "$1" ]]; then
+    echo "Uso: man_newwin <comando>"
+    return 1
+  fi
+
+  osascript <<EOF
+tell application "System Events"
+  tell application process "Terminal"
+    key down option
+    delay 0.1
+    click menu item "Man Page" of menu "Nueva ventana" of menu item "Nueva ventana" of menu "Shell" of menu bar 1
+    delay 0.1
+    key up option
+  end tell
+end tell
+
+tell application "Terminal"
+  set win to front window
+  tell application "Finder"
+    set screenBounds to the bounds of window of desktop
+    set screenWidth to item 3 of screenBounds
+  end tell
+  set bounds of win to {screenWidth - 550, 60, screenWidth - 100, 960}
+
+  do script "LESS='-sR' man $1" in win
+end tell
+EOF
+}
+
+
